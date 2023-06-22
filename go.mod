d1b28a526896c76bde112c9f15e6e62d951049f0
F.Baube <fbaube@iki.fi>
2023-06-22 11:02:38 +0300

    Initial

---
+++ go.mod
@@ -0,0 +1,28 @@
+module github.com/fbaube/rowmodels
+
+go 1.20
+
+require (
+	github.com/fbaube/dsmnd v0.0.0-20230612185132-db1a09c6de38
+	github.com/fbaube/fileutils v0.0.0-20230605132543-0237cda358d6
+	github.com/fbaube/repoutils v0.0.0-20230614203419-8d65ca445082
+)
+
+require (
+	github.com/fatih/color v1.15.0 // indirect
+	github.com/fbaube/ctoken v0.0.0-20230518060940-6f364ff08c9d // indirect
+	github.com/fbaube/logutils v0.0.0-20230607184837-dc04e193943f // indirect
+	github.com/fbaube/miscutils v0.0.0-20230402173202-ac4340addc77 // indirect
+	github.com/fbaube/mlog v0.0.0-20230205182808-c77321da3ff3 // indirect
+	github.com/fbaube/stringutils v0.0.0-20230612185027-a01a68c97d21 // indirect
+	github.com/fbaube/wasmutils v0.0.0-20230206184435-0359d2530836 // indirect
+	github.com/fbaube/xmlutils v0.0.0-20230518061403-2465e549e10a // indirect
+	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mgutz/str v1.2.0 // indirect
+	golang.org/x/net v0.11.0 // indirect
+	golang.org/x/sys v0.9.0 // indirect
+	golang.org/x/tools v0.9.3 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ tbldescr_inbatchrow.go
@@ -0,0 +1,83 @@
+package rowmodels
+
+import (
+	"fmt"
+	D "github.com/fbaube/dsmnd"
+	FU "github.com/fbaube/fileutils"
+	RU "github.com/fbaube/repoutils"
+	// "github.com/fbaube/nurepo/db"
+)
+
+/*
+var TableSummary_ContentityRow = D.TableSummary{
+var TableDescriptor_ContentityRow = TableDescriptor{
+func (cro *ContentityRow) PtrFields() []any { // barfs on []db.PtrFields
+var ColumnSpecs_ContentityRow = []D.ColumnSpec{
+type ContentityRow struct {
+func (p *ContentityRow) String() string {
+*/
+
+// TableSummary_InbatchRow describes the table.
+var TableSummary_InbatchRow = D.TableSummary{
+	D.TABL, "INB", "inbatch", "Input batch of imported files"}
+
+// TableDescriptor_InbatchRow TBS and no foreign keys.
+var TableDescriptor_InbatchRow = RU.TableDescriptor{
+	"inbatch",     // Name
+	"inb",         // ShortName
+	"idx_inbatch", // IDName
+	//
+	"FilCt, RelFP, AbsFP, T_Cre, Descr", // ColumnNames
+	// No foreign keys
+	ColumnSpecs_InbatchRow, // []D.ColumnSpec
+}
+
+// TODO: Still can't sort out the notation for ptr constraints ?!
+func (inbro *InbatchRow) PtrFields() []any { // barfs on []db.PtrFields
+	return []any{&inbro.Idx_Inbatch, &inbro.FilCt, &inbro.RelFP,
+		&inbro.AbsFP, &inbro.T_Cre, &inbro.Descr}
+}
+
+// ColumnSpecs_InbatchRow specifies:
+//   - file count
+//   - two path fields (rel & abs)
+//   - creation time
+//   - description
+//
+// .
+var ColumnSpecs_InbatchRow = []D.ColumnSpec{
+	D.ColumnSpec{D.INTG, "filct",
+		"Nr. of files", "Number of files"}, // D.INTEG
+	D.DD_RelFP,
+	D.DD_AbsFP,
+	D.DD_T_Cre,
+	D.ColumnSpec{D.TEXT, "descr", "Batch descr.", "Inbatch description"},
+	D.DD_T_Cre, // D.DATIM // THIS AND T_Edt SHOULD USE
+	// DEFAULT https://www.sqlite.org/lang_createtable.html#dfltval
+	// CURRENT_TIMESTAMP "YYYY-MM-DD HH:MM:SS"
+	D.ColumnSpec{D.TEXT, "descr",
+		"Batch descr.", "Inbatch description"}, // D.STRNG
+}
+
+// InbatchRow describes (in the DB)
+// a single import batch at the CLI.
+//   - NOTE: Maybe rename this to FileSet ?
+//   - TODO: Maybe represent this with a dsmnd.NSPath: Batch.nr+Path
+type InbatchRow struct {
+	Idx_Inbatch int
+	FilCt       int
+	RelFP       string
+	AbsFP       FU.AbsFilePath
+	T_Cre       string
+	// Batch "name" ? Or add a separate Name field ?
+	Descr string
+}
+
+// TODO Write col desc's using Desmond !
+// TODO Generate ColNames from ColumnSpecs_InbatchRow
+
+// String implements Stringer. FIXME
+func (p *InbatchRow) String() string {
+	return fmt.Sprintf("inbatchrow FIXME")
+	// p.PathProps.String(), p.PathAnalysis.String())
+}
