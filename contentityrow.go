0ddd9e45e6997befd1bb8c3dea552a57224c4265
F.Baube <fbaube@iki.fi>
2023-12-16 23:15:13 +0200

    Overwrite old stuff

---
+++ contentityrow.go
@@ -0,0 +1,129 @@
+package rowmodels
+
+import (
+	"fmt"
+
+	D "github.com/fbaube/dsmnd"
+	FU "github.com/fbaube/fileutils"
+	RU "github.com/fbaube/repoutils"
+	// "github.com/fbaube/nurepo/db"
+)
+
+// TableSummary_ContentityRow summarizes the table.
+var TableSummary_ContentityRow = D.TableSummary{
+    D.BDT_CLXN, "contentity", "cnt", "Content entity"}
+
+// TableDescriptor_ContentityRow specifies 11 DB columns,
+
+// incl primary key (assumed) and one foreign key, "inbatch".
+var TableDescriptor_ContentityRow = RU.TableDescriptor{
+	"contentity",     // Name
+	"cnt",            // ShortName
+	"idx_contentity", // IDName
+	"RelFP, AbsFP, Descr, T_Cre, T_Imp, T_Edt, " +
+		"RawMT, Mimtp, MType, Contt", // ColumnNames
+	// One foreign key: "inbatch"
+	ColumnSpecs_ContentityRow, // []D.ColumnSpecs
+}
+
+// PtrFields implements interface [Row].
+// TODO: Still can't sort out the notation for ptr constraints ?!
+func (cro *ContentityRow) PtrFields() []any { // barfs on []db.PtrFields
+	return []any{
+		&cro.Idx_Contentity, &cro.Idx_Inbatch,
+		&cro.PathProps.RelFP, &cro.PathProps.AbsFP,
+		// &cro.RelFP, &cro.AbsFP,
+		// &cro.FUPP.RelFP, &cro.FUPP.AbsFP,
+		&cro.Descr, &cro.T_Cre, &cro.T_Imp, &cro.T_Edt,
+		&cro.PathProps.TypedRaw.MarkupType,
+		&cro.PathAnalysis.ContypingInfo.MimeType,
+		&cro.PathAnalysis.ContypingInfo.MType,
+		&cro.PathProps.TypedRaw.Raw}
+}
+
+// ColumnSpecs_ContentityRow specifies
+//   - a primary key (actually, it doescNOT - a primary
+//     key is assumed, and handled elsewhere)
+//   - a foreign key "inbatch"
+//   - two path fields (rel & abs)
+//   - three time fields (creation, import, last-edit)
+//   - a description
+//   - three content-type fields (raw markup type, MIME-type, MType)
+//     NOTE: these are persisted in the DB because
+//   - - they are useful in searching thru content
+//   - - they can be expensive to calculate at import time
+//   - - they can be overridden by choices made by users
+//   - the content itself
+//   - (not for now!) XML content type and XML DOCTYPE
+//   - (not for now!) two LwDITA fields (flavor
+//     [xdita,hdita!,mdita]), LwDITA content type)
+//
+// .
+var ColumnSpecs_ContentityRow = []D.ColumnSpec{
+	D.ColumnSpec{/*D.FKEY*/ "FRKEY", "idx_inbatch", "inbatch",
+		"Input batch of imported content"},
+	D.DD_RelFP,
+	D.DD_AbsFP,
+	D.ColumnSpec{D.BDT_TEXT, "descr", "Description",
+		"Content entity description"},
+	D.DD_T_Cre,
+	D.DD_T_Imp,
+	D.DD_T_Edt,
+	D.ColumnSpec{D.BDT_TEXT, "rawmt", "Markup type", "Raw markup type"},
+	D.ColumnSpec{D.BDT_TEXT, "mimtp", "MIME type", "MIME type"},
+	D.ColumnSpec{D.BDT_TEXT, "mtype", "MType", "MType"},
+	D.ColumnSpec{D.BDT_TEXT, "contt", "Content", "Entity raw content"},
+	// D.ColSpec{D.BDT_TEXT, "xmlcontype", "XML contype", "XML content type"},
+	// D.ColSpec{D.BDT_TEXT, "xmldoctype", "XML Doctype", "XML Doctype"},
+	// D.ColSpec{D.BDT_TEXT, "ditaflavor", "LwDITA flavor", "LwDITA flavor"},
+	// D.ColSpec{D.BDT_TEXT, "ditacontype", "LwDITA contype", "LwDITA cnt type"},
+}
+
+// ContentityRow describes (in the DB) the entity's content
+// plus its "dead properties" - basically, properties that
+// are set by the user, rather than calculated as needed.
+// Has the entity Raw content, in [PathProps.TypedRaw.Raw].
+type ContentityRow struct {
+	Idx_Contentity int
+	Idx_Inbatch    int // NOTE: Rename to FILESET? Could be multiple?
+	Descr          string
+	// Times is T_Cre, T_Imp, T_Edt string
+	RU.Times
+	// PathProps has Raw and is // => EntityProps !!
+	// CT.TypedRaw { Raw, SU.MarkupType string };
+	// RelFP, ShortFP string;
+	// FileMeta { os.FileInfo, exists bool, MU.Errer }
+	FU.PathProps
+	// PathAnalysis is a ptr, so that we get a
+	// NPE if it is not initialized properly;
+	// or if analysis failed, if (for example)
+	// the content is too short.
+	// FU.PathAnalysis is
+	// XU.ContypingInfo { FileExt, MimeType, =>
+	//   MimeTypeAsSnift, MType string }
+	// ContentityBasics { XmlRoot, Text, Meta CT.Span; // => TopLevel !!
+	//     MetaFormat string; MetaProps SU.PropSet }
+	// XmlContype string
+	// *XU.ParsedPreamble
+	// *XU.ParsedDoctype
+	// DitaFlavor  string
+	// DitaContype string
+	*FU.PathAnalysis // NEED DETAIL
+	// Contt string
+
+	// For these next two fields, instead put the refs & defs
+	//   into another table that FKEY's into this table.
+	// ExtlLinkRefs // links that point outside this File
+	// ExtlLinkDefs // link targets in-file that are visible outside this File
+	// Linker = an outgoing link
+	// Linkee = the target of an outgoing link
+	// Linkable = a symbol that CAN be a Linkee
+}
+
+// TODO Write col desc's using Desmond !
+// TODO Generate ColNames from ColumnSpecs_ContentityRow
+
+// FIXME: String implements Stringer.
+func (p *ContentityRow) String() string {
+	return fmt.Sprintf("PP<%s> AR <%s>", "", "")
+}
--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module github.com/fbaube/rowmodels
 go 1.20
 
 require (
-	github.com/fbaube/dsmnd v0.0.0-20230612185132-db1a09c6de38
-	github.com/fbaube/fileutils v0.0.0-20230605132543-0237cda358d6
-	github.com/fbaube/repoutils v0.0.0-20230614203419-8d65ca445082
+	github.com/fbaube/dsmnd v0.0.0-20230622205413-6095fd80db31
+	github.com/fbaube/fileutils v0.0.0-20230622205530-1d7f778c2e03
+	github.com/fbaube/repoutils v0.0.0-20230622205932-7aa3d9781632
 )
 
 require (
@@ -14,15 +14,16 @@ require (
 	github.com/fbaube/logutils v0.0.0-20230607184837-dc04e193943f // indirect
 	github.com/fbaube/miscutils v0.0.0-20230402173202-ac4340addc77 // indirect
 	github.com/fbaube/mlog v0.0.0-20230205182808-c77321da3ff3 // indirect
-	github.com/fbaube/stringutils v0.0.0-20230612185027-a01a68c97d21 // indirect
+	github.com/fbaube/sqlite3 v0.0.0-20230622182711-a6140311d5d2 // indirect
+	github.com/fbaube/stringutils v0.0.0-20230622210105-a1af04c1f93d // indirect
 	github.com/fbaube/wasmutils v0.0.0-20230206184435-0359d2530836 // indirect
 	github.com/fbaube/xmlutils v0.0.0-20230518061403-2465e549e10a // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/mgutz/str v1.2.0 // indirect
 	golang.org/x/net v0.11.0 // indirect
 	golang.org/x/sys v0.9.0 // indirect
-	golang.org/x/tools v0.9.3 // indirect
+	golang.org/x/tools v0.10.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
